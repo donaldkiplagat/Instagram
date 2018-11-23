7e20f93a6bdb6c095dd6aa6dddfd784a5fa5443a
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-11-23 15:20:21 +0300

    Registration changed to simple.urls

--- clone/admin.py
+++ clone/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post,Location 
+from .models import Post,Location
 
 # Register your models here.
 
--- instagram/urls.py
+++ instagram/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('clone.urls')),
-    url(r'^accounts/',include('registration.backends.hmac.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
     url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
     url(r'^tinymce',include('tinymce.urls')),
 ]
